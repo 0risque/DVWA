3b3a8c083a013f42ca31219e5027305c32ce3421
0risque <30286755+0risque@users.noreply.github.com>
2022-05-04 23:31:15 +0200

    bash script handling of no DVWA env var

--- main.sh
+++ main.sh
@@ -8,10 +8,12 @@ service mysql start
 sleep 3
 mysql -uroot -pvulnerables -e "CREATE USER 'dvwa'@'%' IDENTIFIED BY 'p@ssw0rd';CREATE DATABASE dvwa;GRANT ALL privileges ON dvwa.* TO 'dvwa'@'%';"
 
-#trick to pass system env var to apache2
-# write env related to DVWA in file
+# trick to pass system env var to apache2
+# write env related to DVWA in a file, then append export to apache2 config file
 eval "env | grep DVWA > /dvwaenv"
-echo "export \$(cat /dvwaenv | xargs)" >> /etc/apache2/envvars
+if [ -s /dvwaenv ] ;then
+    echo "export \$(cat /dvwaenv | xargs)" >> /etc/apache2/envvars
+fi
 
 echo '[+] Starting apache'
 service apache2 start
